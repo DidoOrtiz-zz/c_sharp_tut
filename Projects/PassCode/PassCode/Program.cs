590baa1a76dd8d6eed2f9e1c7fd1a8b70b8bb57e
Diana Lorena Ortíz Libreros <diana.ortiz@Diana-Ortiz-MacBook-Pro.local>
2020-09-09 20:51:52 -0500

    First commit C# Tutorial for Beginners

---
+++ Projects/PassCode/PassCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pass = "";
+
+            while (pass != "secret")
+            {
+                Console.WriteLine("Enter your user");
+                var user = Console.ReadLine();
+
+                Console.WriteLine("Enter your password");
+                pass = Console.ReadLine();
+
+                if (pass != "secret")
+                {
+
+                    Console.WriteLine("Your User or Password are not correct.");
+                }
+
+            }
+
+            Console.WriteLine("You are authenticated in the System.");
+
+        }
+    }
+}
---
+++ Projects/Survey/Survey/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old you are?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine("You birth month is: {0}", month);
+
+            if (month == "march")
+            {
+
+                Console.WriteLine("Your are an Aries.");
+            }
+            else if (month == "april")
+            {
+
+                Console.WriteLine("You are a Taurus.");
+            }
+            else if (month == "may")
+            {
+
+                Console.WriteLine("You are a Gemini.");
+            }
+
+
+
+
+        }
+    }
+}
